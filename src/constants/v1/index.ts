4d175834f34aefbb5c5356562a7b2e15d1a15b50
David Gomadza <welcome@gtps.finance>
2022-09-17 20:12:28 +0100

    DAVID

--- src/constants/v1/index.ts
+++ src/constants/v1/index.ts
@@ -6,7 +6,7 @@ import V1_FACTORY_ABI from './v1_factory.json'
 const V1_FACTORY_ADDRESSES: { [chainId in ChainId]: string } = {
   
   [ChainId.MUMBAI]: '0xae53b10373744870B528735BE0d26F2938C2d4e9',//TODO: DO NOT SUPPORT V1 ON MUMBAI
-  [ChainId.MATIC]: "0xD3E51Ef092B2845f10401a0159B2B96e8B6c3D30"
+  [ChainId.MATIC]: "0x0eddc871615f00c529bb387237f36fcc3eb2b1af"
 }
 
 const V1_FACTORY_INTERFACE = new Interface(V1_FACTORY_ABI)
